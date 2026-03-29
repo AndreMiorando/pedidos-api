be4c9862bfbde37773c227062f2637ea5bd057db
Andre Miorando <and.mio@hotmail.com>
2026-03-29 14:53:48 -0300

    Initial commit

---
+++ src/test/java/com/dev/pedidos/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
